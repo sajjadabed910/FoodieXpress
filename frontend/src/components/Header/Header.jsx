239df00e8de58f09842e47f3404314a702ea1873
sajjadabed910 <sajjadabed910@gmail.com>
2025-08-18 23:56:48 +0600

    Design Changes

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,7 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2> Order your favourite food here</h2>
-            <p>Choose from a diverse menu featuring a delectable array of dishes crafted wuth the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time. </p>
+            <h2> Quick and Tasty Food at Your Fingertips 🍔</h2>
             <button>View Menu</button>
         </div>
     </div>
