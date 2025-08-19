d1a3d5b5018f516e94e4a3778aa3828d2480602f
sajjadabed910 <sajjadabed910@gmail.com>
2025-08-19 20:47:44 +0600

    Few minor design changes

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,8 +6,7 @@ const ExploreMenu = ({category,setCategory}) => {
 
   return (
     <div className='explore-menu' id='explore-menu'> 
-        <h1>Explore Menu</h1>
-        <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time</p>
+        <h2>Menu</h2>
         <div className="explore-menu-list">
             {menu_list.map((item,index)=>{
                 return(
